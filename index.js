361fd8190959ebf6eb07bb07722bc7546278d240
Ed Preston <epreston@prestonsoft.com>
2024-07-10 18:11:43 +1000

    convert to "flat config format"

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-module.exports = {
-  env: {
-    es2022: true,
-    browser: true,
-    node: true,
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['**/dist', '**/public', '**/coverage', 'notes', 'meta'],
   },
-
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
-
-  extends: ['eslint:recommended', 'prettier'],
-
-  ignorePatterns: ['dist', 'public', 'coverage', '/notes', '/meta'],
-
-  rules: {
-    'no-debugger': 'error',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-
-    eqeqeq: ['error', 'smart'],
-
-    semi: 'error',
-    quotes: [
-      'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: true },
-    ],
-    'quote-props': ['error', 'consistent-as-needed'],
-
-    'no-throw-literal': ['error'],
-
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'no-unused-vars': 'off',
-
-    'no-duplicate-imports': 'warn',
-    'no-new-native-nonconstructor': 'error',
-
-    yoda: ['error', 'never', { exceptRange: true }],
-
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'any',
-        ignoreReadBeforeAssign: false,
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
-    ],
+
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+
+    rules: {
+      'no-debugger': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+
+      eqeqeq: ['error', 'smart'],
+      semi: 'error',
+
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true,
+        },
+      ],
+
+      'quote-props': ['error', 'consistent-as-needed'],
+
+      'no-throw-literal': ['error'],
+
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'no-unused-vars': 'off',
+
+      'no-duplicate-imports': 'warn',
+      'no-new-native-nonconstructor': 'error',
+
+      yoda: ['error', 'never', { exceptRange: true }],
+
+      'prefer-const': [
+        'error',
+        {
+          destructuring: 'any',
+          ignoreReadBeforeAssign: false,
+        },
+      ],
+    },
   },
-};
+  eslintConfigPrettier,
+];
