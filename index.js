0c2cd75a305af409efc7219b150271b7da9a1d83
Ed Preston <epreston@prestonsoft.com>
2026-01-13 06:30:26 +1100

    Update index.js
    update ecma version and associated globals

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ export default [
   {
     name: 'epreston/shared/rules',
     languageOptions: {
-      ecmaVersion: 2022,
+      ecmaVersion: 2024,
       sourceType: 'module',
       globals: {
         ...globals.browser,
-        ...globals.es2022,
+        ...globals.es2024,
         ...globals.node,
       },
     },
